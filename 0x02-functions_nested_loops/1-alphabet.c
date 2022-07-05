2958a1bf772172b331e2822a0eb71f108b45b3dd
Sisay Assefa <sasefa9@gmail.com>
2022-07-05 16:26:10 +0300

    I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include <main.h>
+
+/*
+ * print the alphabet, in lowercase, followed by a new line.
+ * you can use _putchar twice in your code
+ *
+ * main returns 0 (success)
+ *
+ * */
+
+void print_alphabet(void){
+	char _char;
+	_char = 'a';
+	while (_char <= 'z')
+	{
+		_putchar(_char);
+		_char++;
+	}
+	
+	_putchar('\n');
+
+}
